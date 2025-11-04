1a9e1195ca5079289ee80a135585e32723732735
thaksheel <tna324@lehigh.edu>
2025-11-03 20:43:46 -0500

    finished cpamm

--- CPAMM.sol
+++ CPAMM.sol
@@ -1,34 +1,122 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19; 
+pragma solidity ^0.8.19;
 
-contract CPAMM{
-    IERC20 public immutable token0; 
-    IERC20 public immutable token1; 
+import "./IERC20.sol";
 
-    uint256 public reserve0; 
-    uint256 public reserve1; 
+contract CPAMM {
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
 
-    uint256 public totalSupply; 
-    mapping (address => uint256) public balanceOf; 
+    uint256 public reserve0;
+    uint256 public reserve1;
 
-    constructor (address _token0, address _token1) {
-        token0 = IERC20(_token0); 
-        token1 = IERC20(_token1); 
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(address _token0, address _token1) {
+        token0 = IERC20(_token0);
+        token1 = IERC20(_token1);
     }
 
     function _mint(address _to, uint256 _amount) private {
-        balanceOf[_to] += _amount; 
-        totalSupply += _amount; 
-
+        balanceOf[_to] += _amount;
+        totalSupply += _amount;
     }
 
-    function _burn (address _from, uint256 _amount) private { 
-        balanceOf[_from] -= _amount; 
-        totalSupply -= _amount; 
-
+    function _burn(address _from, uint256 _amount) private {
+        balanceOf[_from] -= _amount;
+        totalSupply -= _amount;
     }
 
-    function swap(address _tokenIn, uint256 _amountIn) external returns (uni256 amountOut) {
+    function _update(uint256 _reserve0, uint256 _reserve1) private {
+        reserve0 = _reserve0;
+        reserve1 = _reserve1;
+    }
 
+    function _sqrt(uint256 val) private pure returns (uint root) {
+        if (val > 3) {
+            root = val;
+            uint256 x = val / 2 + 1;
+            while (x < root) {
+                root = x;
+                x = (val / x + x) / 2;
+            }
+        } else if (val != 0) {
+            root = 1;
+        }
+    }
+
+    function _min(uint256 x, uint256 y) private pure returns (uint256) {
+        return x <= y ? x : y;
+    }
+
+    function swap(
+        address _tokenIn,
+        uint256 _amountIn
+    ) external returns (uint256 amountOut) {
+        require(
+            _tokenIn == address(token0) || _tokenIn == address(token1),
+            "Invalid token"
+        );
+        bool isToken0 = _tokenIn == address(token0);
+        (
+            IERC20 tokenIn,
+            IERC20 tokenOut,
+            uint256 reserveIn,
+            uint256 reserveOut
+        ) = isToken0
+                ? (token0, token1, reserve0, reserve1)
+                : (token1, token0, reserve1, reserve0);
+        tokenIn.transferFrom(msg.sender, address(this), _amountIn);
+
+        // How much to transfer
+        uint256 amountInWithFee = (_amountIn * 997) / 1000;
+        amountOut =
+            (reserveOut * amountInWithFee) / (reserveIn * amountInWithFee);
+        tokenOut.transfer(msg.sender, amountOut);
+
+        _update(
+            token0.balanceOf(address(this)),
+            token1.balanceOf(address(this))
+        );
+    }
+
+    function addLiquidity(
+        uint256 _amount0,
+        uint256 _amount1
+    ) external returns (uint256 shares) {
+        token0.transferFrom(msg.sender, address(this), _amount0);
+        token1.transferFrom(msg.sender, address(this), _amount1);
+        if (reserve0 > 0 || reserve1 > 0) {
+            require(reserve0 * _amount1 == reserve1 * _amount0);
+        }
+
+        if (totalSupply == 0) {
+            shares = _sqrt(_amount0 * _amount1);
+        } else {
+            shares = _min(
+                (_amount0 * totalSupply) / reserve0,
+                (_amount1 * totalSupply) / reserve1
+            );
+        }
+        require(shares > 0, "shares = 0");
+        _mint(msg.sender, shares);
+        _update(
+            token0.balanceOf(address(this)),
+            token1.balanceOf(address(this))
+        );
+    }
+
+    function removeLiquity(
+        uint256 _shares
+    ) external returns (uint256 amount0, uint256 amount1) {
+        uint256 bal0 = token0.balanceOf(address(this));
+        uint256 bal1 = token1.balanceOf(address(this));
+        amount0 = (_shares * bal0) / totalSupply;
+        amount1 = (_shares * bal1) / totalSupply;
+        _burn(msg.sender, _shares);
+        _update(bal0 - amount0, bal1 - amount1);
+        token0.transfer(msg.sender, amount0);
+        token1.transfer(msg.sender, amount1);
     }
 }
